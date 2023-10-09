607e30b886dfba401c9be24a3991cc444fe4d0a1
韩旭gogogo <js_bin@163.com>
2023-10-09 15:40:56 +0800

    chore: config update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const path = process.env.NODE_ENV === 'production' ? '/natata-admin/' : '/'
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: path,
   plugins: [react()],
 })
