5499f5c9da2133b22879aa7bd42dc6df08c63c68
韩旭gogogo <js_bin@163.com>
2023-10-09 16:34:44 +0800

    chore: routers add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,54 @@
 import { useState } from 'react'
+import { Routes, Route, Outlet, Link } from "react-router-dom";
 
-function App() {
+// === Routers === //
+import Home from './pages/Home'
+import Team from './pages/Team'
+import NoMatch from './pages/404'
 
+const App = () => {
   return (
     <div>
-      test
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="team" element={<Team />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
     </div>
   )
+}
+function Layout() {
+  return (
+    <div>
+      {/* A "layout route" is a good place to put markup you want to
+          share across all the pages on your site, like navigation. */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/team">team</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/nothing-here">Nothing Here</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+
+      {/* An <Outlet> renders whatever child route is currently active,
+          so you can think about this <Outlet> as a placeholder for
+          the child routes we defined above. */}
+      <Outlet />
+    </div>
+  );
 }
 
 export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+// === Components === //
+import { Button } from 'antd'
+
+const Home = () => {
+    return <div>
+        <p>Home</p>
+        <Button type="primary">确认</Button>
+    </div>
+}
+export default Home
