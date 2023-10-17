ef57eff0ba0ddf509e7701afe2c2a7cb5b8389b2
韩旭gogogo <js_bin@163.com>
2023-10-17 15:48:47 +0800

    feature: home page build

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,10 +9,9 @@ import { fetchAbtestConfig } from '@/services/abtest'
 
 
 const Home = () => {
-    const { result } = useAsync(fetchAbtestConfig, [])
-    return <div>
-        <p>Home{JSON.stringify(result)}</p>
-        <Button type="primary">确认</Button>
+    return <div className='flex items-center justify-center h-full'>
+        <img className='w-[150px] h-[150px] rounded-[50%] m-0  p-1' src="https://natata-api.vercel.app/logo.png" alt="" />
+        <p className='text-lg ml-4 font-bold'>Hakuma natata!!!</p>
     </div>
 }
 export default Home
